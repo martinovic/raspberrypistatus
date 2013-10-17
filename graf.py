f5a55959bb32d34a60872d7a0857bd9d5c2675ac
root <root@encom510.(none)>
2013-10-16 23:41:08 -0300

    Se agrega el menu

---
+++ graf.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+import sqlite3 as lite
+
+app = Flask(__name__, static_folder='/var/www/flask/static')
+
+@app.route('/graf')
+@app.route('/<name>')
+def graf(name=None):
+    '''
+    '''
+    con = None
+    con = lite.connect('/home/pi/getDataRPi/datosRPi.db')
+    cur = con.cursor()
+    sql = ("SELECT * FROM temperaturas")
+    cur.execute(sql)
+    data = cur.fetchall()
+    dataList = []
+    for row in data:
+        dictData = {}
+        dictData = {'fecha': row[2], 'temp': row[1]}
+        dataList.append(dictData)
+
+    return render_template('googlegraf.html', datos=dataList)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0')
+
+
+
