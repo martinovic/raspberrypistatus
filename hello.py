3ee006be3bffc724e30d6671e263b133676f9a8c
root <root@encom510.(none)>
2013-10-16 22:51:55 -0300

    Se agregan los graficos

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 import math
 import subprocess
+import sqlite3 as lite
 app = Flask(__name__, static_folder='/var/www/flask/static')
 
 
@@ -136,6 +137,25 @@ def hello(name=None):
         actual=actual, minima=minima, maxima=maxima, governor=governor,
         rx=rx, tx=tx, total=total, posts=posts, storage=store, temper=temper,
         memory=memory)
+
+
+@app.route('/graf')
+def graf(name=None):
+    '''
+    '''
+    con = None
+    con = lite.connect('/home/pi/getDataRPi/datosRPi.db')
+    cur = con.cursor()
+    sql = ("SELECT * FROM temperaturas")
+    cur.execute(sql)
+    data = cur.fetchall()
+    dataList = []
+    for row in data:
+        dictData = {}
+        dictData = {'fecha': row[2], 'temp': row[1]}
+        dataList.append(dictData)
+
+    return render_template('googlegraf.html', datos=dataList)
 
 if __name__ == "__main__":
     app.debug = True
